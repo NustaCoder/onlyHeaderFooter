76a211e0c0023357cc880794a28dba00c511beb3
NustaCoder <46111071+NustaCoder@users.noreply.github.com>
2020-05-16 17:40:44 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,15 +7,15 @@ class Header extends Component {
             <React.Fragment>
                 <Navbar dark>
                     <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarBrand href="/">Famished Restaurant</NavbarBrand>
                     </div>
                 </Navbar>
                 <Jumbotron>
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
-                                <h1>Ristorante con Fusion</h1>
-                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                                <h1>Famished Restaurant</h1>
+                                <p>We take inspiration from the World Ketki paralikar, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses! Because it's not just a food It's a lifetime journey of a girl served on your dish</p>
                             </div>
                         </div>
                     </div>
